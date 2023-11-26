ee8fb11f9896932b6744f9acba1eca3684d565ce
rositta <annroy2708@gmail.com>
2023-11-26 14:45:53 -0500

    contract class

---
+++ src/Contract.java
@@ -0,0 +1,6 @@
+abstract class Contract {
+    // Common properties and methods for all contract types
+    public abstract double calculateSalary(Employee employee);
+
+    public abstract String contractType();
+}
